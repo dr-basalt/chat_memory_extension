431e7453bd568f323712e49f5eee6970b57c4499
dr-basalt <contact@ori3com.cloud>
2025-04-13 00:41:59 +0200

    Add files via upload

--- launcher.sh
+++ launcher.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-# Installer dépendances et lancer les containers
+echo "[INFO] Installation de k3s, docker-compose et dépendances..."
+# Installation script placeholder
+docker-compose -f infra/docker-compose.yml up -d --build
